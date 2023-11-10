aaf45ef01d5247ff2e8d5ff53aef6d968d157ff8
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2023-11-10 22:09:51 +0000

    FEATURE-8: Application should support user-defined parameters

--- cmd/goto/main.go
+++ cmd/goto/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/grafviktor/goto/internal/state"
 	"github.com/grafviktor/goto/internal/storage"
 	"github.com/grafviktor/goto/internal/ui"
+	"github.com/grafviktor/goto/internal/utils"
 	"github.com/grafviktor/goto/internal/version"
 )
 
@@ -24,58 +25,73 @@ var (
 	buildCommit  string
 )
 
-func readSettings() config.UserSettings {
-
-	var userSettings config.UserSettings
-
-	if err := env.Parse(&userSettings); err != nil {
-		fmt.Printf("%+v\n", err)
-	}
-
-	// Read environment variables, fallback to file settings
-	flag.StringVar(&userSettings.HostsFilePath, "f", userSettings.HostsFilePath, "Path to yaml file with the list of hosts")
-	flag.StringVar(&userSettings.LogFilePath, "p", userSettings.LogFilePath, "Log file path")
-	flag.StringVar(&userSettings.LogLevel, "l", userSettings.LogLevel, "Log level: debug, info, none")
-	flag.Parse()
-}
+const appName = "goto"
 
 func main() {
 	// Set application version and build details
 	version.Set(buildVersion, buildDate, buildCommit)
 
-	lg, err := logger.New("goto", logger.LevelDebug)
-	if err != nil {
-		log.Fatalf("Can't create log file %v", err)
+	environmentParams := config.User{}
+	// Command line parameters have higher precedence than other parameters, but lower than command line
+	if err := env.Parse(&environmentParams); err != nil {
+		fmt.Printf("%+v\n", err)
 	}
 
-	lg.Info("Starting application")
-	lg.Debug("Version %s", version.BuildVersion())
-	lg.Debug("Build date %s", version.BuildDate())
-	lg.Debug("Commit %s", version.BuildCommit())
+	commandLineParams := config.User{}
+	// Command line parameters have the highest precedence
+	flag.StringVar(&commandLineParams.AppHome, "d", environmentParams.AppHome, "Application home directory")
+	flag.StringVar(&commandLineParams.LogLevel, "l", environmentParams.LogLevel, "Log verbosity level: debug, info")
+	flag.Parse()
+
+	var err error
+	commandLineParams.AppHome, err = utils.GetAppDir(appName, commandLineParams.AppHome)
+	if err != nil {
+		log.Fatalf("Can't get application home folder: %v", err)
+	}
+
+	// Create application folder
+	if err = utils.CreateAppDirIfNotExists(commandLineParams.AppHome); err != nil {
+		log.Fatalf("Can't create application home folder: %v", err)
+	}
+
+	// Create application logger
+	lg, err := logger.New(commandLineParams.AppHome, commandLineParams.LogLevel)
+	if err != nil {
+		log.Fatalf("Can't create log file: %v", err)
+	}
+
+	// Create application configuration and set application home folder
+	appConfig := config.Merge(environmentParams, commandLineParams, &lg)
+
+	// Logger created. Immediately print application version
+	lg.Info("Start application")
+	lg.Info("Version %s", version.BuildVersion())
+	lg.Info("Build date %s", version.BuildDate())
+	lg.Info("Commit %s", version.BuildCommit())
 
 	ctx := context.Background()
-	appConfig := config.New(ctx, &lg)
+	application := config.NewApplication(ctx, appConfig, &lg)
 
-	hostStorage, err := storage.Get(ctx, appConfig)
+	hostStorage, err := storage.Get(ctx, application)
 	if err != nil {
-		lg.Debug("Error running program: %v", err)
+		lg.Error("Error running application: %v", err)
 		os.Exit(1)
 	}
 
-	appState := state.Get(appConfig.HomeFolder, &lg)
+	appState := state.Get(application.Config.AppHome, &lg)
 	uiComponent := ui.NewMainModel(ctx, hostStorage, appState, &lg)
 	p := tea.NewProgram(&uiComponent, tea.WithAltScreen())
 
-	if _, err := p.Run(); err != nil {
-		lg.Debug("Error running program: %v", err)
+	if _, err = p.Run(); err != nil {
+		lg.Error("Error running application: %v", err)
 		os.Exit(1)
 	}
 
 	lg.Debug("Save application state")
 	err = appState.Persist()
 	if err != nil {
-		lg.Debug("Can't save application state before closing %v", err)
+		lg.Error("Can't save application state before closing %v", err)
 	}
 
-	lg.Debug("Close the application")
+	lg.Info("Close the application")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,91 +2,48 @@ package config
 
 import (
 	"context"
-	"os"
-	"path"
-
-	"github.com/grafviktor/goto/internal/utils"
-	"gopkg.in/yaml.v2"
 )
 
-var appName = "goto"
-var configFileName = "config.yaml"
+// const configFileName = "config.yaml"
 
 type Logger interface {
 	Debug(format string, args ...any)
+	Error(format string, args ...any)
 	Close()
 }
-type UserSettings struct {
-	logger         Logger
-	configFilePath string
-	homeFolder     string
-	HostsFilePath  string `env:"HOSTS_FILE" yaml:"host_file,omitempty"`
-	LogLevel       string `env:"LOG_LEVEL"  yaml:"log_level,omitempty"`
-	LogFilePath    string `env:"LOG_PATH"   yaml:"log_path, omitempty"`
+
+type User struct {
+	AppHome  string `yaml:"-" env:"GG_HOME"`
+	LogLevel string `yaml:"log_level,omitempty" env:"GG_LOG_LEVEL" envDefault:"info"`
 }
 
-func NewUserSettings(logger Logger) (UserSettings, error) {
-	us := UserSettings{logger: logger}
-
-	appHome, err := utils.GetAppDir(logger, appName)
-	if err != nil {
-		logger.Debug("Failed to get application home folder%v\n", err)
-
-		return us, err
+func Merge(envParams, cmdParams User, logger Logger) User {
+	// TODO: Can use reflection to override envParams with cmdParams, instead of addressing exact fields
+	if len(cmdParams.AppHome) > 0 {
+		envParams.AppHome = cmdParams.AppHome
 	}
+	logger.Debug("Set application home folder to %s\n", envParams.AppHome)
 
-	configFilePath := path.Join(appHome, configFileName)
-
-	logger.Debug("Read application configuration from %s\n", configFilePath)
-	fileData, err := os.ReadFile(configFilePath)
-	if err != nil {
-		logger.Debug("Can't read application configuration %v\n", err)
-
+	if len(cmdParams.LogLevel) > 0 {
+		envParams.LogLevel = cmdParams.LogLevel
 	}
+	logger.Debug("Set application log level to %s\n", envParams.LogLevel)
 
-	us.homeFolder = appHome
-
-	err = yaml.Unmarshal(fileData, &us)
-	if err != nil {
-		us.logger.Debug("Can't read parse application configuration %v\n", err)
-
-		return us, err
-	}
-
-	return us, nil
-
-}
-
-func (us *UserSettings) Save() error {
-	result, err := yaml.Marshal(us)
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(us.configFilePath, result, 0o600)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func New(ctx context.Context, logger Logger, settings UserSettings) Application {
-
-	config := Application{
-		Context:      ctx,
-		Logger:       logger,
-		AppName:      appName,
-		UserSettings: settings,
-	}
-
-	return config
+	return envParams
 }
 
 type Application struct {
-	HomeFolder string
-	AppName    string
-	Context    context.Context
-	Logger     Logger
-	UserSettings
+	Context context.Context
+	Logger  Logger
+	Config  User
+}
+
+func NewApplication(ctx context.Context, userConfig User, logger Logger) Application {
+	app := Application{
+		Context: ctx,
+		Config:  userConfig,
+		Logger:  logger,
+	}
+
+	return app
 }
