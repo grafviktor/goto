eff7e3a8051d52ec37423364656f8b2ee8212455
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2023-12-10 00:36:14 +0000

    IMPROVEMENT-23: Add mock objects and increase unit test coverage

--- internal/ui/component/hostlist/list_test.go
+++ internal/ui/component/hostlist/list_test.go
@@ -13,33 +13,18 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafviktor/goto/internal/model"
-	"github.com/grafviktor/goto/internal/storage"
 	"github.com/grafviktor/goto/internal/utils"
 )
 
-func getTestListModel() *listModel {
-	// Create a new host
-	h := model.NewHost(0, "", "", "localhost", "root", "id_rsa", "2222")
-
-	// Add three items to the list
-	items := []list.Item{ListItemHost{h}, ListItemHost{h}, ListItemHost{h}}
-
-	// Create listModel using constructor function (using 'New' is important to preserve hotkeys)
-	lm := New(context.TODO(), nil, nil, nil)
-	lm.innerModel.SetItems(items)
-
-	return &lm
-}
-
 func Test_ListTitleUpdate(t *testing.T) {
 	// Create a lm with initial state
-	lm := *getTestListModel()
+	lm := *NewMockListModel(false)
 
 	// Select host
 	lm.innerModel.Select(0)
 
 	// Create a message of type msgFocusChanged
-	msg := msgFocusChanged{}
+	msg := msgRefreshUI{}
 	// Apply the function
 	lm = lm.listTitleUpdate(msg)
 
@@ -50,56 +35,51 @@ func Test_listModel_Change_Selection(t *testing.T) {
 	tests := []struct {
 		name                   string
 		expectedSelectionIndex int
-		model                  listModel
 		tea.KeyMsg
 	}{
 		// Simulate focus next event
 		{
 			"Select next using 'j' key",
 			2,
-			*getTestListModel(),
+
 			tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'j'}},
 		},
 		{
 			"Select next using '↓' key",
 			2,
-			*getTestListModel(),
 			tea.KeyMsg{Type: tea.KeyDown},
 		},
 		{
 			"Select next using 'tab' key",
 			2,
-			*getTestListModel(),
 			tea.KeyMsg{Type: tea.KeyTab},
 		},
 		// Simulate focus previous event
 		{
 			"Select previous using 'k' key",
 			0,
-			*getTestListModel(),
 			tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'k'}},
 		},
 		{
 			"Select previous using '↑' key",
 			0,
-			*getTestListModel(),
 			tea.KeyMsg{Type: tea.KeyUp},
 		},
 		{
 			"Select previous using 'shift+tab' key",
 			0,
-			*getTestListModel(),
 			tea.KeyMsg{Type: tea.KeyShiftTab},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			model := *NewMockListModel(false)
 			// Select item at index 1. We need this preselection in order
 			// to test 'focus previous' and 'focus next' messages
-			tt.model.innerModel.Select(1)
+			model.innerModel.Select(1)
 
 			// Receive updated model
-			updatedModel, _ := tt.model.Update(tt.KeyMsg)
+			updatedModel, _ := model.Update(tt.KeyMsg)
 
 			// Check if the selected index is correct
 			if lm, ok := updatedModel.(listModel); ok {
@@ -132,7 +112,7 @@ func Test_BuildProcess(t *testing.T) {
 	require.Error(t, err)
 
 	// Test case: Item is selected
-	listModel := getTestListModel()
+	listModel := NewMockListModel(false)
 	cmd, err := listModel.buildProcess(&stdErrorWriter{})
 	require.NoError(t, err)
 
@@ -144,7 +124,7 @@ func Test_BuildProcess(t *testing.T) {
 
 func Test_RunProcess(t *testing.T) {
 	// Mock data for listModel
-	listModel := getTestListModel()
+	listModel := NewMockListModel(false)
 
 	errorWriter := stdErrorWriter{}
 
@@ -175,22 +155,167 @@ func Test_RunProcess(t *testing.T) {
 	// require.Equal(t, "", string(errorWriter.err))
 }
 
-type mockRepo struct {
+// When remove mode is enabled, test confirm action event.
+// Once confirmed, the item should be removed rom the list.
+// However, we can't check whether the item was really deleted
+// from the database, as we would have to wait while
+func Test_removeItem(t *testing.T) {
+	tests := []struct {
+		name          string
+		model         listModel
+		mode          string
+		want          interface{}
+		preselectItem int
+		expectedItems int
+	}{
+		{
+			name:          "Remove item success",
+			model:         *NewMockListModel(false),
+			mode:          modeRemoveItem,
+			want:          tea.BatchMsg{},
+			expectedItems: 2,
+		},
+		{
+			name:          "Remove item error because of the database error",
+			model:         *NewMockListModel(true),
+			mode:          modeRemoveItem,
+			want:          msgErrorOccured{},
+			expectedItems: 3,
+		},
+		{
+			name:          "Remove item error wrong item selected",
+			model:         *NewMockListModel(false),
+			mode:          modeRemoveItem,
+			want:          msgErrorOccured{},
+			preselectItem: 10,
+			expectedItems: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Preselect item
+			tt.model.innerModel.Select(tt.preselectItem)
+			// Set mode removeMode
+			tt.model.mode = tt.mode
+			// Call remove function
+			_, cmd := tt.model.removeItem()
+			// Expected to be tea.Batch, because when removing host we trigger extra commands
+			require.IsType(t, tt.want, cmd(), "Wrong message type")
+			// Get all items from the database without error
+			items, _ := tt.model.repo.GetAll()
+			// Make sure that the list contains expected quantity of ite,s after remove operation
+			require.Equal(t, tt.expectedItems, len(items))
+		})
+	}
+}
+
+func Test_confirmAction(t *testing.T) {
+	// Create a new model. There is no special mode (for instance remove item mode)
+	model := NewMockListModel(false)
+	// Imagine that user triggers confirm aciton
+	updatedModel, cmd := model.confirmAction()
+	// When cancel action, we reset mode and return back to normal state
+	require.Len(t, updatedModel.mode, 0)
+	// Updated model should not be nil
+	require.NotNil(t, updatedModel)
+	// Because there is no active mode, model should ignore the event
+	require.Nil(t, cmd)
+
+	// Create a new model
+	model = NewMockListModel(false)
+	// Now we enable remove mode
+	model.mode = modeRemoveItem
+	// Imagine that user triggers confirm aciton
+	updatedModel, cmd = model.confirmAction()
+	// When confirm action is triggered, we reset mode and return back to normal state
+	require.Len(t, updatedModel.mode, 0)
+	// Updated model should not be nil
+	require.NotNil(t, updatedModel)
+	// cmd should not be nil because when we modify storage, some events will be dispatched
+	// we should not check the exact event type here, because it is action-dependent
+	require.NotNil(t, cmd)
+}
+
+func Test_enterRemoveItemMode(t *testing.T) {
+	// Create a new model
+	model := *NewMockListModel(false)
+	// Select non-existent index
+	model.innerModel.Select(10)
+	// Call enterRemoveItemMode function
+	model, cmd := model.enterRemoveItemMode()
+	// and make sure that mode is unchanged
+	require.Len(t, model.mode, 0)
+	// cmd() should return msgErrorOccured error
+	require.IsType(t, msgErrorOccured{}, cmd(), "Wrong message type")
+
+	// Create another model
+	model = *NewMockListModel(false)
+	// Select a first item, which is valid
+	model.innerModel.Select(0)
+	// Call enterRemoveItemMode function
+	model, cmd = model.enterRemoveItemMode()
+	// Ensure that we entered remove mode
+	require.Equal(t, modeRemoveItem, model.mode)
+	// md() should return msgRefreshUI in order to update title
+	require.IsType(t, msgRefreshUI{}, cmd(), "Wrong message type")
+}
+
+// ================================================ MOCKS ========================================== //
+
+// ============================================== List Model
+
+func NewMockListModel(storageShouldFail bool) *listModel {
+	storage := NewMockStorage(storageShouldFail)
+
+	// Create listModel using constructor function (using 'New' is important to preserve hotkeys)
+	lm := New(context.TODO(), storage, nil, nil)
+
+	items := make([]list.Item, 0)
+	// Wrap hosts into List items
+	hosts := storage.hosts
+	for _, h := range hosts {
+		items = append(items, ListItemHost{Host: h})
+	}
+
+	lm.innerModel.SetItems(items)
+
+	return &lm
+}
+
+// =============================================== Storage
+
+func NewMockStorage(shouldFail bool) *mockStorage {
+	hosts := []model.Host{
+		model.NewHost(0, "", "", "localhost", "root", "id_rsa", "2222"),
+		model.NewHost(0, "", "", "localhost", "root", "id_rsa", "2222"),
+		model.NewHost(0, "", "", "localhost", "root", "id_rsa", "2222"),
+	}
+
+	return &mockStorage{
+		shouldFail: shouldFail,
+		hosts:      hosts,
+	}
+}
+
+type mockStorage struct {
 	shouldFail bool
+	hosts      []model.Host
 }
 
 // Delete implements storage.HostStorage.
-func (mr mockRepo) Delete(id int) error {
-	if mr.shouldFail {
+func (ms *mockStorage) Delete(id int) error {
+	if ms.shouldFail {
 		return errors.New("Mock error")
 	}
+
+	ms.hosts = append(ms.hosts[:id], ms.hosts[id+1:]...)
 
 	return nil
 }
 
 // Get implements storage.HostStorage.
-func (mr mockRepo) Get(hostID int) (model.Host, error) {
-	if mr.shouldFail {
+func (ms *mockStorage) Get(hostID int) (model.Host, error) {
+	if ms.shouldFail {
 		return model.Host{}, errors.New("Mock error")
 	}
 
@@ -198,102 +323,19 @@ func (mr mockRepo) Get(hostID int) (model.Host, error) {
 }
 
 // GetAll implements storage.HostStorage.
-func (mr mockRepo) GetAll() ([]model.Host, error) {
-	if mr.shouldFail {
-		return []model.Host{}, errors.New("Mock error")
+func (ms *mockStorage) GetAll() ([]model.Host, error) {
+	if ms.shouldFail {
+		return ms.hosts, errors.New("Mock error")
 	}
 
-	return []model.Host{}, nil
+	return ms.hosts, nil
 }
 
 // Save implements storage.HostStorage.
-func (mr mockRepo) Save(model.Host) error {
-	if mr.shouldFail {
+func (ms *mockStorage) Save(model.Host) error {
+	if ms.shouldFail {
 		return errors.New("Mock error")
 	}
 
 	return nil
-}
-
-/*
-func Test_confirmAction(t *testing.T) {
-	// Set up the initial state with mode set to "modeRemoveItem"
-	model := getTestListModel()
-	model.mode = modeRemoveItem
-	// msg := tea.KeyMsg{Type: tea.KeyDown},
-	msg := tea.KeyMsg{
-		Type:  tea.KeyRunes,
-		Runes: []rune{'d'},
-	}
-
-	model.repo = mockRepo{}
-	// Call the confirmAction function
-	newModel, cmd := model.confirmAction(msg)
-
-	// Assert that the mode has been reset and removeItem function was called
-	require.Equal(t, "", newModel.mode, "Expected mode to be reset")
-
-	// Expected to be tea.Batch, because when removing host we trigger extra commands
-	require.IsType(t, tea.Batch(), cmd, "Expected to be tea.Batch")
-}
-*/
-
-func Test_removeItem(t *testing.T) {
-	tests := []struct {
-		name         string
-		model        listModel
-		mode         string
-		repo         storage.HostStorage
-		want         interface{}
-		selectedItem int
-	}{
-		{
-			name:         "Remove item success",
-			model:        *getTestListModel(),
-			repo:         mockRepo{},
-			mode:         modeRemoveItem,
-			want:         tea.BatchMsg{},
-			selectedItem: 0,
-		},
-		{
-			name:         "Remove item error because of the database error",
-			model:        *getTestListModel(),
-			repo:         mockRepo{shouldFail: true},
-			mode:         modeRemoveItem,
-			want:         msgErrorOccured{},
-			selectedItem: 0,
-		},
-		{
-			name:         "Remove item error wrong item selected",
-			model:        *getTestListModel(),
-			repo:         mockRepo{shouldFail: false},
-			mode:         modeRemoveItem,
-			want:         msgErrorOccured{},
-			selectedItem: 10,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			msg := tea.KeyMsg{
-				Type:  tea.KeyRunes,
-				Runes: []rune{'d'},
-			}
-
-			// Set mode removeMode
-			tt.model.mode = tt.mode
-			// Setup database
-			tt.model.repo = tt.repo
-			// Select item
-			tt.model.innerModel.Select(tt.selectedItem)
-			// Call the confirmAction function
-			newModel, cmd := tt.model.confirmAction(msg)
-
-			// Assert that the mode has been reset and removeItem function was called. That's happening in any case
-			// independtently whether the operation was succesfull or not.
-			require.Equal(t, "", newModel.mode, "Expected removeMode to be cleared")
-
-			// Expected to be tea.Batch, because when removing host we trigger extra commands
-			require.IsType(t, tt.want, cmd(), "Wrong message type")
-		})
-	}
 }
