fb3142ae932a5f65b4ce9d94d2faa392b24aacc7
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2024-04-30 23:58:52 +0100

    IMPROVEMENT-39: Format imports

--- internal/model/ssh/option_test.go
+++ internal/model/ssh/option_test.go
@@ -1,9 +1,10 @@
 package ssh
 
 import (
-	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_ConstructKeyValueOption(t *testing.T) {
