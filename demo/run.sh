f1b09b769664e1d93c014234649b3846410efdcf
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2024-10-10 00:34:42 +0100

    DOCUMENT: Update project demos

--- demo/run.sh
+++ demo/run.sh
@@ -2,8 +2,18 @@
 
 cd $(dirname "$(readlink -f "$0")")
 
+export VHS_PUBLISH=false # disable "Host your GIF on vhs.charm.sh: vhs publish <file>.gif" message
 export GG_HOME=.
 export GG_LOG_LEVEL=debug
 
-vhs use_custom_config.tape
-vhs goto_and_tmux.tape
+function cleanup() {
+    rm hosts.yaml state.yaml app.log 2>/dev/null
+}
+
+cleanup
+for file in *.tape; do
+    basename=${file%.*}
+    cp "${basename}.yaml" hosts.yaml 2>/dev/null
+    vhs "$file" > /dev/null
+    cleanup
+done
