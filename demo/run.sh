ae962e234700140fe7823dfc0af8d841d13edefd
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2024-10-14 22:35:31 +0100

    DOCUMENT: Adjustments in *.tape files

--- demo/run.sh
+++ demo/run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-cd $(dirname "$(readlink -f "$0")")
+set -e
+
+cd "$(dirname "$(readlink -f "$0")")"
 
 export VHS_PUBLISH=false # disable "Host your GIF on vhs.charm.sh: vhs publish <file>.gif" message
 export GG_HOME=.
 export GG_LOG_LEVEL=debug
 
 function cleanup() {
-    rm hosts.yaml state.yaml app.log 2>/dev/null
+    rm -f hosts.yaml state.yaml app.log 2>/dev/null
 }
 
 cleanup
 for file in *.tape; do
     basename=${file%.*}
-    cp "${basename}.yaml" hosts.yaml 2>/dev/null
+    if [ -f "${basename}.yaml" ]; then
+        cp "${basename}.yaml" hosts.yaml
+    fi
     vhs "$file" > /dev/null
     cleanup
 done
