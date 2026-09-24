536ee65f6962b37868ebd27ddaffdb6fc08d9bb2
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2026-09-24 09:39:51 +0100

    FEATURE-155: Increase unit test coverage. In progress

--- internal/ui/component/sshsession/session_test.go
+++ internal/ui/component/sshsession/session_test.go
@@ -53,6 +53,48 @@ func TestUpdate(t *testing.T) {
 	}
 }
 
+func TestUpdate_WindowSizeMsg(t *testing.T) {
+	// Test that terminal which we're wrapping will get its size
+	// with status line height substracted. Testing both cases
+	// when create and update terminal.
+	m, _ := New(80, 24, "hostname")
+	require.Equal(t, 80, m.term.Width())
+	require.Equal(t, 22, m.term.Height())
+
+	m.Update(tea.WindowSizeMsg{Width: 100, Height: 50})
+	require.Equal(t, 100, m.term.Width())
+	require.Equal(t, 48, m.term.Height())
+}
+
+/*
++ func (m *Model) handleMouseMsg(msg tea.MouseMsg) (tea.Model, tea.Cmd) {
++ 	if msg.Mouse().Y > m.term.Height() {
++ 		// Skip mouse events in the status area.
++ 		return m, nil
++ 	}
++
+  - updated, cmd := m.term.Update(msg)
+  - m.term = updated
+  - return m, cmd
+    }
+*/
+func TestUpdate_MouseMsg(t *testing.T) {
+	t.Skip("Skipping as it's failing")
+	// Test that if we click on a position outside the terminal area, the mouse event is ignored.
+	m, _ := New(80, 24, "hostname")
+	// Remember, the terminal height is lower, than the whole component height, because of the
+	// status line.
+	require.Equal(t, 22, m.term.Height())
+	// Now click one pixel below the terminal
+	_, cmd := m.Update(tea.MouseClickMsg{Button: tea.MouseLeft, X: 10, Y: 23})
+	// It'll return null
+	require.Nil(t, cmd)
+	// Now click on the last row of the terminal
+	_, cmd = m.Update(tea.MouseClickMsg{Button: tea.MouseLeft, X: 10, Y: 22})
+	// The message will be handled by the termview and we should get non-nil cmd
+	require.NotNil(t, cmd)
+}
+
 func TestHandleSessionClose(t *testing.T) {
 	m, _ := New(80, 24, "hostname")
 	cmd := m.handleSessionClose(termview.ClosedMsg{})
